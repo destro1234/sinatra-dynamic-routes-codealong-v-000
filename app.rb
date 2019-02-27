5f678bf5fb0b402ab3b1b1d168383aabbfebcc5f
Elias Ventura <eliasventura23@gmail.com>
2019-02-27 23:50:19 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require_relative 'config/environment'
+require 'pry'
 
 class App < Sinatra::Base
 
@@ -22,6 +23,7 @@ class App < Sinatra::Base
   get "/multiply/:num1/:num2" do
     @product = params[:num1] * params[:num2]
     "#{@product}"
+    binding.pry
   end
 
 end
